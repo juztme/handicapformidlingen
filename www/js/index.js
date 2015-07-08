69276ea3993f64e6d932bc8eb1ec5b9035b5bcb1
Rohde Fischer <rohdef@rohdef.dk>
2015-07-08 20:25:27 +0200

    Fix nativedroid

--- www/js/index.js
+++ www/js/index.js
@@ -17,35 +17,35 @@
  * under the License.
  */
 (function($) {
-var app = {
+  var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+      this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+      document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+      app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = $('#suicide');
+      var parentElement = document.getElementById(id);
+      var listeningElement = $('#suicide');
 
-        listeningElement.html("murder!!!!");
+      listeningElement.html("murder!!!!");
 
-        console.log('Received Event: ' + id);
+      console.log('Received Event: ' + id);
     }
-};
+  };
 
-app.initialize();
+  app.initialize();
 })(jQuery);
